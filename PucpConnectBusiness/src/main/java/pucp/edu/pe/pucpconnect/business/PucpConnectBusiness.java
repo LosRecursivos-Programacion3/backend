aa135ebc051e9e5be25879921c823faf4e03ddca
danielC128 <danielcastillorios811@gmail.com>
2025-06-02 19:58:05 -0500

    Mergeo con la rama principal

--- PucpConnectBusiness/src/main/java/pucp/edu/pe/pucpconnect/business/PucpConnectBusiness.java
+++ PucpConnectBusiness/src/main/java/pucp/edu/pe/pucpconnect/business/PucpConnectBusiness.java
@@ -4,13 +4,32 @@
 
 package pucp.edu.pe.pucpconnect.business;
 
+import pucp.edu.pe.pucpconnect.business.impl.AlumnoServiceImpl;
+import pucp.edu.pe.pucpconnect.domain.Usuarios.Alumno;
+import pucp.edu.pe.pucpconnect.persistence.BaseDAO;
+import pucp.edu.pe.pucpconnect.persistence.daoimpl.Usuarios.AlumnoDAOImpl;
+
 /**
  *
  * @author Irico
  */
 public class PucpConnectBusiness {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {    
+        BaseDAO<Alumno> alumnoDAO = new AlumnoDAOImpl(); 
+        AlumnoService alumnoService = new AlumnoServiceImpl(alumnoDAO);
+
+        Alumno nuevoAlumno = new Alumno();
+        nuevoAlumno.setNombre("Pedro");
+        nuevoAlumno.setEdad(20);
+        nuevoAlumno.setEmail("pedro@pucp.edu.pe");
+        //nuevoAlumno.setCarrera(null);
+
+        try {
+            alumnoService.registrarAlumno(nuevoAlumno);
+            System.out.println("Alumno registrado correctamente");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
